79986315cf8944940499cef3006f954850e04c2a
Renatta Pereira <renatta.lll@hotmail.com>
2022-11-11 17:26:55 -0300

    variavel e if

---
+++ exemplo.js
@@ -0,0 +1,10 @@
+const valor1 = 1;
+const valor2 = 2;
+const valor3 = 3;
+const valores = 6;
+
+if (valor1 >= valor2) {
+  console.log("Verdadeiro, valor1 é maior que valor2");
+} else {
+  console.log("Falso, valor1 é menor que valor2");
+}
